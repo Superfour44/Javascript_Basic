bd271eebf0bdc370508a0cbb2a7f8ec9de06d487
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-09 00:07:01 +0700

    pattern show data on web

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-document.write("Hello World");
+
+document.write("<h1>Hello World</h1>");
+document.write("<p>hiiiiiiiiiiii</p>");
+
+
+
+alert("Warning");
+console.log("Hello Jvascrip");
+console.error("Virussss");
+console.warn("bug");
