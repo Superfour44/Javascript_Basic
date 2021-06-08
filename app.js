01af4b9a1c8adca38c15fce90515c1aa8382ad31
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-09 00:10:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-
+// show data on web
 document.write("<h1>Hello World</h1>");
 document.write("<p>hiiiiiiiiiiii</p>");
 
 
-
+//show alert message
 alert("Warning");
+
+//show text in console
 console.log("Hello Jvascrip");
 console.error("Virussss");
 console.warn("bug");
