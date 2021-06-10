a19631412e958d304966d54f6163a6e68a7a2800
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-10 23:10:34 +0700

    learn array

---
+++ array/app.js
@@ -0,0 +1,11 @@
+let number = Array(1,2,3,4,5,6); //solution1
+let colors = ["red","pink","green","white"];//solution2
+
+// index start at 0 = member no.1
+console.log(number);
+console.log("Array index 0 =",number[0])
+console.log("Array index 3 =",number[3])
+
+console.log(colors);
+colors[1]="yellow";
+console.log(colors[1])
