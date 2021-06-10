d578f89f536051ab4ce56d7462748371dc29bdd3
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-10 15:45:15 +0700

    statement show on web

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+console.log("Hello");
+document.getElementById("text").innerHTML = "Superfour";
+document.write("Hello Document Write");
+alert("Alert Box");
+
 // show data on web
-document.write("<h1>Hello World</h1>");
-document.write("<p>hiiiiiiiiiiii</p>");
+// document.write("<h1>Hello World</h1>");
+// document.write("<p>hiiiiiiiiiiii</p>");
 
 
-//show alert message
-alert("Warning");
+// //show alert message
+// alert("Warning");
 
-//show text in console
-console.log("Hello Jvascrip");
-console.error("Virussss");
-console.warn("bug");
+// //show text in console
+// console.log("Hello Jvascrip");
+// console.error("Virussss");
+// console.warn("bug");
