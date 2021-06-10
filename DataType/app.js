0f165c30ddbd56c206d8a11cf7d93494649936c5
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-10 16:12:37 +0700

    learn data type

---
+++ DataType/app.js
@@ -0,0 +1,10 @@
+let money = 500;//integer
+let price = 99.99;//float
+let message = "Hello"; //string
+let chkname = true;//bool
+let age = "20"+10;//concat string
+console.log(typeof(money));
+console.log(price);
+console.log(message);
+console.log(chkname);
+console.log(age);
