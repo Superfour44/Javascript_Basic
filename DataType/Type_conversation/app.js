618cec204d343dacc0dbefb263e55a1f28475a44
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-10 22:27:55 +0700

    Update app.js

---
+++ DataType/Type_conversation/app.js
@@ -0,0 +1,9 @@
+// let age = parseInt("20");//convert string to int
+let age = +"20";//convert string to int
+// let price = 100.00.toString();//float to string
+let price = 100+"";//float to string
+
+console.log(age);
+console.log(typeof(age))
+console.log(price);
+console.log(typeof(price));
