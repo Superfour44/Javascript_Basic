88e02002ced99a5227de7c432f218875e4e4a33e
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-11 00:20:31 +0700

    Update app.js

--- array/Operator/app.js
+++ array/Operator/app.js
@@ -1,7 +1,15 @@
-console.log("Plus",5+10);
-console.log("Minus",20-10);
-console.log("Multiple",5*10);
-console.log("Divide",20/2);
-console.log("Mod",20%9);
+// console.log("Plus",5+10);
+// console.log("Minus",20-10);
+// console.log("Multiple",5*10);
+// console.log("Divide",20/2);
+// console.log("Mod",20%9);
 
-console.log("Power",5**2);
+// console.log("Power",5**2);
+let a = 5,b = 10;
+
+console.log("1",a == b);
+console.log("2",a != b);
+console.log("3",a > b);
+console.log("4",a < b);
+console.log("5",a >= b);
+console.log("6",a <= b);
