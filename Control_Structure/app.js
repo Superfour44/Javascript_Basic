7137a06c6f554df0c4671fc145276a967cd956ef
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-12 22:24:11 +0700

    learn control structure

---
+++ Control_Structure/app.js
@@ -0,0 +1,31 @@
+let score = 30;
+
+//score > 40 = D
+//score > 50 = C
+//score > 60 = B
+//score > 70 = A
+
+
+if(score > 70){
+    console.log("Grade:A")
+}else if(score > 60){
+    console.log("Grade:B")
+}else if(score > 50){
+    console.log("Grade:C")
+}else if(score > 40){
+    console.log("Grade:D")
+}else{
+    console.log("Grade:F")
+}
+
+// let balance = 5000;
+// let withdraw = 10000;
+
+
+// if(withdraw <= balance){
+//     console.log("Withdraw Process");
+//     balance -= withdraw;
+//     console.log("Total Money",balance);
+// }else{
+//     console.log("Withdraw More than Money Total")
+// }
