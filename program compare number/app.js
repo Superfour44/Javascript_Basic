3d49000c17065004a91acdb2c540d9db3c2df4ab
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-13 00:05:46 +0700

    Program compare number

---
+++ program compare number/app.js
@@ -0,0 +1,13 @@
+//program compare number
+
+let x =50;
+let y =50;
+
+
+if(x>y){
+    console.log("X have value is most");
+}else if(x<y){
+    console.log("Y have value is most");
+}else{
+    console.log("x = y");
+}
