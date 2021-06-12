d3585505608318eb9526eb84008189508ed2cb5d
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-12 23:05:41 +0700

    learn with education case

---
+++ Control_Structure/multiple if/app.js
@@ -0,0 +1,11 @@
+let age =13;
+
+if(age<=15){
+    if(age==15){
+        console.log("grade 3")
+    }else if(age==14){
+        console.log("grade 2")
+    }else if(age==13){
+        console.log("grade 1")
+    }
+}
