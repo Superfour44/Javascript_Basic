ff5b7f0dd45927d1a1de601c50b609aef62de369
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-12 22:23:26 +0700

    learn  logical opearator

---
+++ Logical_operator/app.js
@@ -0,0 +1,22 @@
+//ตัดเกรด
+//70-80 = A
+//50-60 = B
+//<49 = f
+
+
+// let score = 79
+// console.log(score >= 70 && score < 80),"A";
+// console.log(score >= 50 && score <= 60),"B";
+
+//prefix
+let a = 5,b=10
+
+console.log("strat value A:",a);
+console.log("Prefix A:",++a)
+console.log("Current value A:",a)
+
+a=5;
+// postfix
+console.log("strat value A:",a);
+console.log("Postfix A:",a++)
+console.log("Current value A:",a)
