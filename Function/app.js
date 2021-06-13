77345eb3983a372b3e77f6451429d583daac9fbc
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-13 22:41:42 +0700

    learn function in javascript

---
+++ Function/app.js
@@ -0,0 +1,26 @@
+function print_hello_world(){
+    console.log("Hello world");
+}
+// print_hello_world();
+function message(){
+    alert("Alert Meassage");
+}
+
+function display_name(){
+    document.write("Superfour");
+}
+
+//argument = parameter is send
+//parameter = parameter is receive 
+
+
+function show_number(x){
+    console.log("number is:",x);
+}
+
+function fullname(fname,lname){
+    console.log("First Name:",fname,"Last Name:",lname)
+}
+let number = 40;
+show_number(number);
+fullname("Four","Super")
