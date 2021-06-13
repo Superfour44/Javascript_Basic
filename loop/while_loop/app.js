e2944e1f3df0b864d88340bc7d9773088705f424
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-13 20:36:04 +0700

    Learn while loop

---
+++ loop/while_loop/app.js
@@ -0,0 +1,13 @@
+// while(condition = true){
+//     //what doing?
+// }
+
+
+let count = 1;
+while(count<=5){
+    console.log("Prpduct number is:",count);
+        if(count==3){
+            break;
+        }
+    count++;
+}
