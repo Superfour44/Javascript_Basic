7267d3f9bc6fe2ce19d8f5b6b416325f40640c98
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-15 00:17:02 +0700

    learn convert array to string
    this code learn convert array to string and learn pop function

---
+++ array/convert_array_toString/app.js
@@ -0,0 +1,9 @@
+let colors = ["red","white","blue","yellow","purple"]
+// let x = colors.toString();//convert array to string
+// let x = colors.join(" ");//convert array to string change "," -> " "
+// console.log(x);
+
+// console.log("Before pop",colors);
+let x = colors.pop();//x = member of array remove
+console.log(x);
+// console.log("After pop",colors);
