97b3bbdd0b3679ddc30dea293514ad950ebf935b
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-14 23:08:38 +0700

    learn for loop for array member

---
+++ array/for_loop_array/app.js
@@ -0,0 +1,9 @@
+let color = ["red","white","blue","yellow","purple"]
+color.push("black");
+let count = color.length;
+console.log(count);
+
+//for loop
+// for (let i = 0;i<count;i++){
+//     console.log("member of array:",(i+1),color[i])
+// }
