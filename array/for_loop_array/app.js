458be793af1f1dc1a6896ecfcbbb134deb5dd4b6
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-15 00:01:07 +0700

    use foreach in array

--- array/for_loop_array/app.js
+++ array/for_loop_array/app.js
@@ -1,9 +1,16 @@
 let color = ["red","white","blue","yellow","purple"]
 color.push("black");
 let count = color.length;
-console.log(count);
+// console.log(count);
 
 //for loop
 // for (let i = 0;i<count;i++){
 //     console.log("member of array:",(i+1),color[i])
 // }
+
+//for each
+color.forEach(getColor);//arrayname.forEach(FunctionName);
+
+function getColor(colorname) {
+    console.log("color is :",colorname);
+}
