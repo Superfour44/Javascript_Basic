63d2c325fe4382cf4012eb21740fc6602f892831
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-14 16:39:52 +0700

    learn array properties and function

---
+++ array/array_properties/app.js
@@ -0,0 +1,19 @@
+let color = ["red","white","blue","yellow","purple"]
+
+let fruit = ["mango","apple","banana","durian"]
+
+let count = color.length;//check number of array member
+// console.log("before sort:",color);
+// let result = color.sort();//sort array member
+// console.log(count);
+// console.log("After Sort:",result);
+
+// console.log(fruit.sort());
+// console.log(fruit.reverse());//sort by reverse
+
+console.log(fruit[0]);//first member of array
+console.log(fruit[fruit.length-1]);//last member of array
+
+console.log("Before push:",color);
+color.push("grey");
+console.log("After push:",color);
