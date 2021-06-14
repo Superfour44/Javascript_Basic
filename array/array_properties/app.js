fd37313d756c7e1e8005c1aeead2af48c6ad8dbd
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-15 00:41:13 +0700

    sort number

--- array/array_properties/app.js
+++ array/array_properties/app.js
@@ -2,6 +2,8 @@ let color = ["red","white","blue","yellow","purple"]
 
 let fruit = ["mango","apple","banana","durian"]
 
+let points = [20,100,-100,5,-25,10];
+
 let count = color.length;//check number of array member
 // console.log("before sort:",color);
 // let result = color.sort();//sort array member
@@ -17,3 +19,17 @@ console.log(fruit[fruit.length-1]);//last member of array
 console.log("Before push:",color);
 color.push("grey");
 console.log("After push:",color);
+
+
+//sort number 
+
+points.sort(function(a,b) {
+    return a-b;
+})
+
+console.log(points);//less to more
+
+points.sort(function(a,b) {
+    return b-a;
+})
+console.log(points);//more to less
