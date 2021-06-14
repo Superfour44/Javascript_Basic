c7f39003fcfe19f224a7807ac1e9e40b1089522f
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-14 16:13:07 +0700

    Update app.js

--- variable/app.js
+++ variable/app.js
@@ -3,7 +3,21 @@ name = "hello"
 let age = 20;
 
 const tax = 0.07;
-console.log(name);
-console.log(tax);
+// console.log(name);
+// console.log(tax);
 document.write(name)
-console.log("age:",age);
+// console.log("age:",age);
+
+
+//global variable
+let a = 100;
+
+function display() {
+    let b = 50;
+    let a = 20;
+    console.log("A value in fnc =",a)
+    console.log("b value in fnc =",b)
+}
+
+console.log("A value not in fnc =",a)
+display();
