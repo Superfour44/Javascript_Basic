1cd00b9069b102d0f9c739d2a2fae34401fb420b
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-15 23:37:10 +0700

    learn confirm dialog

---
+++ array/confirm_dialog/app.js
@@ -0,0 +1,8 @@
+function deleteData() {
+    let result = confirm("Do you want delete?");
+    if (result){
+        console.log("delete success");
+    }else{
+        console.log("Not delete data");
+    }
+}
