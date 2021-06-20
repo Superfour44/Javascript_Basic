4d67eff9a26142105bf115888e52c586cb73e9e7
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-20 14:34:39 +0700

    learn DOM Document
    solution mix htnl css js

--- HTML_DOM/html_element/app.js
+++ HTML_DOM/html_element/app.js
@@ -1,15 +1,31 @@
-let tag_p = document.getElementById("demo");
-let b = document.querySelector('.myjs');
-let c = document.querySelector('p');
+// const tag_p = document.getElementById("demo");
+// const b = document.querySelector('.myjs');
 
-console.log(b);
-console.log(c);
-let x = 10;
-let y = 20;
+const title = document.getElementById('title');
+const contentEl = document.querySelector('.content');
+const allEl = document.querySelectorAll('p');
+const box = document.querySelector('.box')
 
-function displayText() {
-    tag_p.innerHTML = "show data from parameter x :"+x+" & y "+y;
-    // tag_p.innerHTML = "<strong>Learn javascript<strong>"//change html & text
-    // tag_p.innerText = "Learn javascript"//change text only
+console.log(allEl);
+// let x = 10;
+// let y = 20;
+
+// function displayText() {
+
+//     box.setAttribute('class','dark');
+//     // title.style.color="red";
+//     // title.style.backgroundColor="yellow";
+//     // title.style.fontSize="60px";
+//     // contentEl.setAttribute('class','ChangeNameClass');
+//     // tag_p.innerHTML = "show data from parameter x :"+x+" & y "+y;
+//     // tag_p.innerHTML = "<strong>Learn javascript<strong>"//change html & text
+//     // tag_p.innerText = "Learn javascript"//change text only
+// }
+
+function LightMode(){
+    box.setAttribute('class','light');
+}
+function NightMode(){
+    box.setAttribute('class','dark');
 }
 
